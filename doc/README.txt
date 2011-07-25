8d3720d5d9ca5391e72287bfcd901bab7a93d955
sboye <info@example.com>
2011-07-25 08:25:29 +0000

    First release : owezflow 0.1
    git-svn-id: http://svn.projects.ez.no/owezflow@1 43c4af59-95bd-408d-8cbb-cc17ea863246

---
+++ doc/README.txt
@@ -0,0 +1,24 @@
+-------------------------
+owEZFlow :
+-------------------------
+
+owEZFlow extends ezFlow extension.
+
+With owEZFlow, you can :
+ - define custom attributes even if ManualAddingOfItems is enabled.
+ - define custom labels for custom attributes
+ - define "object relation" custom attributes
+ 
+-------------------------
+Usage
+-------------------------
+
+See "settings/block.ini.append.php" for instructions.
+
+
+
+---------------------------------------------
+Hope you find it useful! 
+
+Simon Boyer
+
---
+++ settings/block.ini.append.php
@@ -0,0 +1,42 @@
+<?php /* #?ini charset="utf-8"?
+
+#[Example]
+#-----------------------------
+# Basics ezFlow parameters
+#-----------------------------
+#Name=Bloc example
+#NumberOfValidItems=4
+#NumberOfArchivedItems=0
+#ManualAddingOfItems=enabled
+
+#-----------------------------
+# List of attributes :
+#-----------------------------
+#CustomAttributes[]=string_example
+#CustomAttributes[]=checkbox_example
+#CustomAttributes[]=node_example
+
+#-----------------------------
+# Attributes type (checkbox|string|text)
+#-----------------------------
+#CustomAttributeTypes[checkbox_example]=checkbox
+
+#-----------------------------
+# BrowseMode (to define an "object relation" attribute)
+#-----------------------------
+#UseBrowseMode[node_example]=true
+
+#-----------------------------
+# Custom labels
+#-----------------------------
+#CustomAttributeLabel[node_example]=Example of a node relation
+#CustomAttributeLabel[string_example]=Example of a string parameter
+
+#-----------------------------
+# Other ezFlow parameters
+#-----------------------------
+#AllowedClasses[]=class_example
+#ViewList[]=embed
+#ViewName[embed]=Embed
+
+*/ ?>
