5ecd5bd728d700f74253a3217c08c58e9fe89c0e
sboye <info@example.com>
2011-08-22 13:08:41 +0000

    Add view cache workflow
    git-svn-id: http://svn.projects.ez.no/owezflow@4 43c4af59-95bd-408d-8cbb-cc17ea863246

--- doc/README.txt
+++ doc/README.txt
@@ -5,14 +5,15 @@ owEZFlow :
 owEZFlow extends ezFlow extension.
 
 With owEZFlow, you can :
+ - set a worflow to update view cache of objects containing eZFlow blocks relating to the published object.
  - define custom attributes even if ManualAddingOfItems is enabled.
  - define custom labels for custom attributes
  - define "object relation" custom attributes
  
+ 
 -------------------------
 Usage
 -------------------------
-
 See "settings/block.ini.append.php" for instructions.
 
 
---
+++ eventtypes/event/ezflowupdate/ezflowupdatetype.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once( 'kernel/common/template.php' );
+
+class ezflowUpdateType  extends eZWorkflowEventType
+{
+    const WORKFLOW_TYPE_STRING = "ezflowupdate";
+    const HTML_CONTENT_TYPE = 'text/html';
+
+    var $site_ini;
+
+    function __construct()
+    {
+        $this->eZWorkflowEventType( self::WORKFLOW_TYPE_STRING, ezpI18n::tr( 'extension/owezflow/workflow/event', 'eZFlow related Update' ) );
+        $this->setTriggerTypes( array( 'content' => array( 'publish' => array( 'after') ) ) );
+        $this->site_ini = eZIni::instance('site.ini');
+    }
+
+
+    function execute( $process, $event )
+    {
+
+    	$parameters = $process->attribute( 'parameter_list' );
+    	
+	    $db = eZDB::instance();
+	    $resArray = $db->arrayQuery( "SELECT ezm_block.node_id FROM ezm_pool, ezm_block WHERE ezm_pool.block_id=ezm_block.id AND ezm_pool.object_id='".$parameters['object_id']."'" );
+	
+	    foreach($resArray as $res) {
+	    	$obj=eZContentObject::fetchByNodeID( $res['node_id']);
+	    	eZContentCacheManager::clearObjectViewCache( $obj->ID, true, false );
+	    }
+
+        return eZWorkflowType::STATUS_ACCEPTED;
+    }
+
+}
+
+eZWorkflowEventType::registerEventType( ezflowUpdateType::WORKFLOW_TYPE_STRING, 'ezflowUpdateType' );
+
+?>
